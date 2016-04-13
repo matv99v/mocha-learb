5dbb260d12b26a47a6059b60e5c704c337a74205
Vladimir Matveev <matv99v@gmail.com>
2016-04-14 00:10:16 +0300

    init

---
+++ src/Timer.js
@@ -0,0 +1,38 @@
+'use strict';
+
+class Timer {
+    constructor(accumulatedTime) {
+        this.isPlaying = false;
+        this.timeStamp = 0;
+        this.accumulatedTime = accumulatedTime || 0;
+    }
+
+    start() {
+        if (!this.isPlaying) {
+            this.timeStamp = Date.now();
+        }
+        this.isPlaying = true;
+    }
+
+    stop() {
+        if (this.isPlaying) {
+            this.isPlaying = false;
+            this.accumulatedTime += (Date.now() - this.timeStamp);
+            this.timeStamp = 0;
+        }
+    }
+
+    getSpentTime() {
+        if (this.isPlaying) {
+            return Date.now() - this.timeStamp + this.accumulatedTime;
+        }
+        return this.accumulatedTime;
+    }
+
+    clear() {
+        this.timeStamp = Date.now();
+        this.accumulatedTime = 0;
+    }
+}
+
+module.exports = Timer;
