a4700c3a374255bdd0bcc4ff95ef36e364d117df
Vladimir Matveev <matv99v@gmail.com>
2016-04-14 22:42:34 +0300

    added validation tests

--- test/validators-test.js
+++ test/validators-test.js
@@ -2,8 +2,6 @@ const assert     = require('chai').assert;
 const validators = require('../src/validator-matveev').validateRegistrationData;
 const FIXTURES   = require('./fixtures.json');
 
-console.log( validators( {phone: '-5a'} ) );
-
 suite('Validate registration data');
 
 Object.keys(FIXTURES).forEach(field => {
